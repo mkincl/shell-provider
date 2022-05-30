4a082dbc64c59f5613024fe40622c7517f5a951c
Carl Smedstad <carl.smedstad@protonmail.com>
2022-05-30 22:35:22 +0200

    Introduce overrideable Make variable FILES_SHELL

--- include.mk
+++ include.mk
@@ -15,18 +15,25 @@ lint: lint-$(NAME)
 .PHONY: fix fix-$(NAME)
 fix: fix-$(NAME)
 
-# Actual targets
+# Which files to act on. This is overrideable.
+FILES_SHELL = $(shell shfmt -f .)
+
+.PHONY: has-files-shell
+has-files-shell:
+	@for file in $(FILES_SHELL); do exit 0; done; echo "FILES_SHELL is empty"; exit 1
+
+# Specific targets
 .PHONY: lint-$(NAME)-shfmt lint-$(NAME)-shellcheck
 lint-$(NAME): lint-$(NAME)-shfmt lint-$(NAME)-shellcheck
 
-lint-$(NAME)-shfmt:
-	shfmt -d . > /dev/null || shfmt -l .
+lint-$(NAME)-shfmt: has-files-shell
+	shfmt -d $(FILES_SHELL) > /dev/null || shfmt -l .
 
-lint-$(NAME)-shellcheck:
-	shfmt -f . | xargs --no-run-if-empty shellcheck
+lint-$(NAME)-shellcheck: has-files-shell
+	shellcheck $(FILES_SHELL)
 
 .PHONY: fix-$(NAME)-shfmt
 fix-$(NAME): fix-$(NAME)-shfmt
 
-fix-$(NAME)-shfmt:
-	shfmt -w .
+fix-$(NAME)-shfmt: has-files-shell
+	shfmt -w $(FILES_SHELL)
